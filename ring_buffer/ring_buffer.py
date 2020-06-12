a62945abcd76f2463dd50e08fa4c8217d4553933
jbell1991 <joseph.louis.bell@gmail.com>
2020-06-12 12:04:53 -0400

    completed ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,40 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.ring = []
+        self.index = 0
 
     def append(self, item):
-        pass
+        if len(self.ring) < self.capacity:
+            self.ring.append(item)
+        else:
+            # replaces the oldest value in the ring
+            self.ring[self.index] = item
+            self.index += 1
+            # resets the index once it exceeds the capacity
+            if self.index + 1 > self.capacity:
+                self.index = 0
 
     def get(self):
-        pass
+        return [i for i in self.ring if i is not None]
+
+# Tests from Readme
+
+# buffer = RingBuffer(3)
+
+# print(buffer.capacity)
+
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+# print(buffer.get())
+
+# buffer.append('d')
+
+# print(buffer.get())
+
+# buffer.append('e')
+# buffer.append('f')
+
+# print(buffer.get())
